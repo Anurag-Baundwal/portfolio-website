97d712cfaec42ba901f6838c0dfab62dbf281b40
Anurag Baundwal <abaundwal@gmail.com>
2023-08-26 19:22:25 +0530

    Initial Commit

---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "Sticky Notes",
+    subtitle: "React and Tailwind",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./sticky-notes.png",
+    link: "https://sticky-notes-by-anurag.netlify.app/",
+  },
+  {
+    title: "Tic Tac Toe",
+    subtitle: "A React App",
+    description:
+      "An app that shows understanding of state and hooks etc",
+    image: "./tic-tac-toe.png",
+    link: "https://tictactoe-by-anurag.netlify.app/",
+  },
+  {
+    title: "Weather App",
+    subtitle: "A React App",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "https://reactbootcamp.com",
+  },
+  {
+    title: "Github Profile",
+    subtitle: "My Github Profile",
+    description:
+      "I work on projects outside of the domain of web development too. You can find them here.",
+    image: "./github-logo.png",
+    link: "https://github.com/Anurag-Baundwal.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "GraphQL",
+  "Material UI",
+];
